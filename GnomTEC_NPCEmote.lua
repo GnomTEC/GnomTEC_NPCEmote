e103c854027c22ae1ad6c443edb9ecfe540a03de
GnomTEC <info@gnomtec.de>
2019-01-05 13:47:29 +0100

    * Updated TOC for WoW patch 8.1.0 (BfA) * fixed GnomTECWidgetEditBox focus issue when clicking in area where is no line yet (indirect through GnomTEC Framework)

--- GnomTEC_NPCEmote.lua
+++ GnomTEC_NPCEmote.lua
@@ -1,10 +1,10 @@
 ﻿-- **********************************************************************
 -- GnomTEC NPCEmote
--- Version: 8.0.1.21
+-- Version: 8.1.0.22
 -- Author: Peter Jack
 -- URL: http://www.gnomtec.de/
 -- **********************************************************************
--- Copyright © 2013-2018 by Peter Jack
+-- Copyright © 2013-2019 by Peter Jack
 --
 -- Licensed under the EUPL, Version 1.1 only (the "Licence");
 -- You may not use this work except in compliance with the Licence.
@@ -28,14 +28,14 @@ local L = LibStub("AceLocale-3.0"):GetLocale("GnomTEC_NPCEmote")
 local addonInfo = {
 	["Name"] = "GnomTEC NPCEmote",
 	["Description"] = L["L_DESCRIPTION"],	
-	["Version"] = "8.0.1.21",
-	["Date"] = "2018-07-16",
+	["Version"] = "8.1.0.22",
+	["Date"] = "2019-01-05",
 	["Author"] = "Peter Jack",
 	["Email"] = "info@gnomtec.de",
 	["Website"] = "http://www.gnomtec.de/",
-	["Copyright"] = "© 2013-2018 by Peter Jack",
+	["Copyright"] = "© 2013-2019 by Peter Jack",
 	["License"] = "European Union Public Licence (EUPL v.1.1)",	
-	["FrameworkRevision"] = 12
+	["FrameworkRevision"] = 13
 }
 
 -- ----------------------------------------------------------------------
