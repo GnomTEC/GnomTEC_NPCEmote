d999391f5e772a2253c636eee6224595a34a39fc
Effie Flammfix <github@gnomtec.de>
2022-06-01 12:21:31 +0200

    * Updated TOC for WoW patch 9.2.5

--- GnomTEC_NPCEmote.lua
+++ GnomTEC_NPCEmote.lua
@@ -1,6 +1,6 @@
 ﻿-- **********************************************************************
 -- GnomTEC NPCEmote
--- Version: 9.2.0.24
+-- Version: 9.2.5.24
 -- Author: Peter Jack
 -- URL: http://www.gnomtec.de/
 -- **********************************************************************
@@ -28,8 +28,8 @@ local L = LibStub("AceLocale-3.0"):GetLocale("GnomTEC_NPCEmote")
 local addonInfo = {
 	["Name"] = "GnomTEC NPCEmote",
 	["Description"] = L["L_DESCRIPTION"],	
-	["Version"] = "9.2.0.24",
-	["Date"] = "2022-03-12",
+	["Version"] = "9.2.5.24",
+	["Date"] = "2022-06-01",
 	["Author"] = "Peter Jack",
 	["Email"] = "info@gnomtec.de",
 	["Website"] = "http://www.gnomtec.de/",
